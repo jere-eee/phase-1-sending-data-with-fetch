368655df258ba9776679b493e82dc1cb90bea407
Akanle Jeremy <akanlejeremy@gmail.com>
2024-10-03 11:47:01 +0300

    fetching

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+}
+
+fetch('http://localhost:3000/dogs', configurationObject)
+  .then(res => res.json())
+  .then(obj => console.log(obj))
+
+function submitData(username, useremail) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: username, email: useremail})
+    })
+        .then(res => res.json())
+        .then((data) => {
+            document.querySelector('body').append(data.id)
+        })
+        .catch((err) => {
+            alert('Error: Wrong execution.');
+            document.querySelector('body').append(err.message)
+        })
+}
